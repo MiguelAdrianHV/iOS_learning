540429a7ddbbfbe66c3ad35c61098b7b1834d41a
MiguelHernandezDexcom <112583722+MiguelHernandezDexcom@users.noreply.github.com>
2023-10-24 16:50:36 -0700

    Added tableview Section and Row implemented

--- TableView/Controller/ViewController.swift
+++ TableView/Controller/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
-    private var movies: [Movie] = Movie.movies
+    private class Section {
+        let title: String
+        let movies: [Movie]
+        
+        init(title: String, movies: [Movie]) {
+            self.title = title
+            self.movies = movies
+        }
+    }
+    
+    private var sections = [Section]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,26 +29,58 @@ class ViewController: UIViewController {
         self.title = "Movies"
         tableView.dataSource = self
         tableView.delegate = self
+        
+        generateMovies()
     }
     
     
 }
 
+private extension ViewController {
+    
+    func generateMovies() {
+        sections.append(Section(title: "A", movies: Movie.moviesWithA))
+        sections.append(Section(title: "B", movies: Movie.moviesWithB))
+        sections.append(Section(title: "C", movies: Movie.moviesWithC))
+        sections.append(Section(title: "D", movies: Movie.moviesWithD))
+        
+        // Una vex obtenido el contenido a mostrar en el TableView
+        // Tenemos que decirle al TableView que el contenido esta listo para mostrarse
+        tableView.reloadData()
+    }
+}
+
+
+// MARK: DataSource
 extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count // Regresar el numero de secciones disponibles.
+    }
     
     // Metodos obligatorios para que funcione un Tableview
     // Metodo para mostrar el numero de elementos en una seccion
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // La seccion empieza como 0 en vez de 1. Donde 0 es igual a 1 y 1 es igual.
-        return movies.count
+        let sectionIndex = section
+        return sections[sectionIndex].movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Metodo que va a instanciar la celda con el identificador agregado
         let cell = tableView.dequeueReusableCell(withIdentifier: "RowForSectionACell") as! RowForSectionACell
-        cell.configureCell(with: Movie.movies[indexPath.row])
+        
+        // sections[Numero de seccion].movie[Numero de pelicula en la seccion]
+        let movie = sections[indexPath.section].movies[indexPath.row]
+        cell.configureCell(with: movie)
         return cell
+    }
+    
+    // 1er Paso para mostrar la seccion del titulo
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sectionIndex = section
+        return sections[sectionIndex].title
     }
 }
 
@@ -47,11 +89,27 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Movie Selected: \(movies[indexPath.row].name)")
+//        print("Movie Selected: \(movies[indexPath.row].name)")
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
+    
+    // 2ndo paso para mostrar la seccion del titulo
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36
+    }
+    
+    // Metodo para cambiar el tema (background, font, etc) de la seccion
+    // note: Metodo opcional.
+    // Tercer paso para mostrar la seccion con el titulo
+    // Descomentar para ver la customizacion del section
+    //    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    //        if let headerView = view as? UITableViewHeaderFooterView {
+    //            headerView.contentView.backgroundColor = .green
+    //            headerView.textLabel?.textColor = .white
+    //        }
+    //    }
 }
 
--- TableView/Model/Movie.swift
+++ TableView/Model/Movie.swift
@@ -15,9 +15,27 @@ struct Movie {
 }
 
 extension Movie {
-    static let movies: [Movie] = [
-        Movie(name: "Green Book", actor: "Viggo Mortensen", type: "Adventure", imageURL: "green_book_portada"),
-        Movie(name: "Toy Story", actor: "Woody", type: "Animation", imageURL: "toy_story_portada"),
-        Movie(name: "Dracula", actor: "Ricardo Ferreti", type: "Miedo", imageURL: "dracula_portada")
+    static let moviesWithA: [Movie] = [
+        Movie(name: "Anatomy", actor: "Viggo Mortensen", type: "Adventure", imageURL: "green_book_portada"),
+        Movie(name: "Action", actor: "Woody", type: "Animation", imageURL: "toy_story_portada"),
+        Movie(name: "Alexander", actor: "Ricardo Ferreti", type: "Miedo", imageURL: "dracula_portada")
+    ]
+    
+    static let moviesWithB: [Movie] = [
+        Movie(name: "Booking", actor: "Viggo Mortensen", type: "Adventure", imageURL: "green_book_portada"),
+        Movie(name: "Billy Hook", actor: "Woody", type: "Animation", imageURL: "toy_story_portada"),
+        Movie(name: "Belinda", actor: "Ricardo Ferreti", type: "Miedo", imageURL: "dracula_portada")
+    ]
+    
+    static let moviesWithC: [Movie] = [
+        Movie(name: "Calefaction", actor: "Viggo Mortensen", type: "Adventure", imageURL: "green_book_portada"),
+        Movie(name: "Center", actor: "Woody", type: "Animation", imageURL: "toy_story_portada"),
+        Movie(name: "Cars", actor: "Ricardo Ferreti", type: "Miedo", imageURL: "dracula_portada")
+    ]
+    
+    static let moviesWithD: [Movie] = [
+        Movie(name: "Diddi", actor: "Viggo Mortensen", type: "Adventure", imageURL: "green_book_portada"),
+        Movie(name: "Dexter", actor: "Woody", type: "Animation", imageURL: "toy_story_portada"),
+        Movie(name: "Dembest", actor: "Ricardo Ferreti", type: "Miedo", imageURL: "dracula_portada")
     ]
 }
